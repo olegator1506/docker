fc3e48d822e03ca0d767c3ce18c3cee085e23f7e
Oleg Artemenko <olegator1506@gmail.com>
2020-12-08 09:19:08 +0700

    Save changes

--- kb51-site/perl-fcgi/run.sh
+++ kb51-site/perl-fcgi/run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker run  --rm -v $(pwd)/share:/share -p 8999:8999 --name site-kb site-kb/perl-fcgi 
+docker run  -it --rm -v /data/web/html/main:/usr/share/nginx/html:ro -p 8998:8999  --net site-kb --name perl-fcgi site-kb/perl-fcgi
